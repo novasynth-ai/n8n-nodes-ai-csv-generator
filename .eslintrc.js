c0a6cb4f12bcc8fc5ace51f2d84282c0a5c88ee7
novasynth-ai <djch72@gmail.com>
2025-07-23 01:20:02 +0930

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,37 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-    extraFileExtensions: ['.json'],
-  },
-  plugins: [
-    '@typescript-eslint',
-    'n8n-nodes-base',
-  ],
-  extends: [
-    'eslint:recommended',
-    '@typescript-eslint/recommended',
-    'plugin:n8n-nodes-base/nodes',
-  ],
-  rules: {
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/no-explicit-any': 'warn',
-    'n8n-nodes-base/node-param-default-missing': 'error',
-    'n8n-nodes-base/node-param-description-missing': 'error',
-  },
-  ignorePatterns: ['dist/**', 'node_modules/**'],
+	root: true,
+	env: {
+		browser: false,
+		es6: true,
+		node: true,
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: 'tsconfig.json',
+		sourceType: 'module',
+		extraFileExtensions: ['.json'],
+	},
+	plugins: [
+		'@typescript-eslint',
+	],
+	extends: [
+		'eslint:recommended',
+		'@typescript-eslint/recommended',
+		'plugin:n8n-nodes-base/nodes',
+	],
+	rules: {
+		'@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+		'@typescript-eslint/no-explicit-any': 'warn',
+		'@typescript-eslint/no-non-null-assertion': 'warn',
+		'@typescript-eslint/prefer-nullish-coalescing': 'error',
+		'@typescript-eslint/prefer-optional-chain': 'error',
+		'@typescript-eslint/no-floating-promises': 'error',
+		'@typescript-eslint/await-thenable': 'error',
+	},
+	ignorePatterns: [
+		'dist/**',
+		'node_modules/**',
+		'*.js',
+	],
 };
 
